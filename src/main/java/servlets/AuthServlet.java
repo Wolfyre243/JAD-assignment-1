01eeb99083926d052ac3d4d2ecb7754005f37816
Wolfyre243 <wolfyreblueflare@gmail.com>
2025-11-12 10:28:11 +0800

    feat: added register controller and route

--- src/main/java/servlets/AuthServlet.java
+++ src/main/java/servlets/AuthServlet.java
@@ -47,19 +47,22 @@ class AuthController {
       response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An error occurred during login.");
     }
   }
-  
-  public static void register(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-  	final HttpSession session = request.getSession();
-  	
-  	// Get form fields
-  	final String email = request.getParameter("email");
+
+  public static void register(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    final HttpSession session = request.getSession();
+
+    // Get form fields
+    final String email = request.getParameter("email");
     final String password = request.getParameter("password");
-    
+    final int roleId = Integer.parseInt(request.getParameter("role_id"));
+
     try {
-    	
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
+      User.createUser(email, password, roleId);
+    } catch (Exception e) {
+      e.printStackTrace();
+      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An error occurred during registration.");
+    }
   }
 }
 
@@ -86,7 +89,7 @@ public class AuthServlet extends HttpServlet {
     if (path.endsWith("/auth/login")) {
       AuthController.login(request, response);
     } else if (path.endsWith("/auth/register")) {
-      // AuthController.register(request, response);
+      AuthController.register(request, response);
     } else {
       response.sendError(HttpServletResponse.SC_NOT_FOUND, "The requested resource was not found.");
     }
